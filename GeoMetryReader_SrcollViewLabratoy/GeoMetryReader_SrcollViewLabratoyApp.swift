92c7e3ae933d3c88793cf9662079522b96a9138c
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2024-03-17 01:27:38 +0100

    Initial Commit

---
+++ GeoMetryReader_SrcollViewLabratoy/GeoMetryReader_SrcollViewLabratoyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoMetryReader_SrcollViewLabratoyApp.swift
+//  GeoMetryReader_SrcollViewLabratoy
+//
+//  Created by Omid Shojaeian Zanjani on 17/03/24.
+//
+
+import SwiftUI
+
+@main
+struct GeoMetryReader_SrcollViewLabratoyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
